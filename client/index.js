2a1306d9f346534c73ac568f5a90ed8afbcaec29
Kazato Sugimoto <uiureo@gmail.com>
2015-12-03 00:24:02 +0900

    Set up env

---
+++ client/index.js
@@ -0,0 +1,10 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const App = () => {
+  return (
+    <p>hello world</p>
+  )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'))
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+mongoose.connect('mongodb://localhost/test')
 
-app.get('/', (req, res) => {
-  res.send('hello')
+const db = mongoose.connection
+
+const itemSchema = mongoose.Schema({
+  body: String
 })
 
-app.listen(3000)
+const Item = mongoose.model('Item', itemSchema)
+
+app.use(require('body-parser').json())
+app.use(express.static('public'))
+
+const router = express.Router()
+
+router.get('/items', (req, res) => {
+  Item.find().then((items) => {
+    res.json(items.map((item) => item.toObject()))
+  })
+})
+
+router.post('/items', (req, res) => {
+  const item = new Item(req.body)
+
+  item.save().then(() => {
+    res.json(item)
+  }).catch((err) => {
+    console.log(err.toString())
+  })
+})
+
+app.use('/api', router)
+
+db.once('open', () => {
+  app.listen(3000)
+})
