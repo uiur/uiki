1a61cb66beb1a21309b82b06e8afa53c13e6dc58
Kazato Sugimoto <uiureo@gmail.com>
2015-12-03 01:34:00 +0900

    Add react-router

--- client/index.js
+++ client/index.js
@@ -1,8 +1,15 @@
 require('es6-promise').polyfill()
 
 const React = require('react')
-const ReactDOM = require('react-dom')
+const { render } = require('react-dom')
+
+const { Router, Route } = require('react-router')
+const { createHistory } = require('history')
 
 const App = require('./components/app')
 
-ReactDOM.render(<App/>, document.querySelector('#app'))
+render((
+  <Router history={ createHistory({ queryKey: false }) }>
+    <Route path='/' component={ App } />
+  </Router>
+), document.querySelector('#app'))
