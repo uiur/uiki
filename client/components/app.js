5709dd0b061d4c672f371d8384a4408eedcbd9dd
Kazato Sugimoto <uiureo@gmail.com>
2015-12-03 01:22:33 +0900

    Add test input

--- client/components/app.js
+++ client/components/app.js
@@ -6,26 +6,53 @@ class App extends React.Component {
     super()
 
     this.state = {
+      value: '',
       items: []
     }
   }
 
   componentDidMount () {
-    request.get('/api/items')
+    this.fetch()
+  }
+
+  fetch () {
+    return request.get('/api/items')
       .then((res) => {
         this.setState({ items: res.data })
       }).catch(console.error.bind(console))
   }
 
+  createItem (data) {
+    return request.post('/api/items', data)
+  }
+
+  onKeyDown (e) {
+    if (e.key === 'Enter') {
+      this.createItem({ body: e.target.value })
+        .then((res) => {
+          this.setState({ value: '' })
+          return this.fetch()
+        })
+    }
+  }
+
   render () {
     return (
-      <ul>
-        {
-          this.state.items.map((item) => {
-            return <li>{ item.body }</li>
-          })
-        }
-      </ul>
+      <main>
+        <input type='text'
+               autoFocus
+               value={ this.state.value }
+               onChange={ (e) => this.setState({ value: e.target.value }) }
+               onKeyDown={ this.onKeyDown.bind(this) } />
+
+        <ul>
+          {
+            this.state.items.map((item) => {
+              return <li>{ item.body }</li>
+            })
+          }
+        </ul>
+      </main>
     )
   }
 }
